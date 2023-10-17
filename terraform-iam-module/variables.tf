5b071c3a6f9a9df9b6f472dd47efe91d9414deb3
TobiasLiu1990 <tobias.liu@live.se>
2023-10-17 08:33:02 +0000

    a

--- terraform-iam-module/variables.tf
+++ terraform-iam-module/variables.tf
@@ -1,14 +1,17 @@
 variable "alias" {
     description = "AWS account alias"
     type = string
+    default = "your aws account"
 }
 
 variable "pw_length" {
     description = "Enter minimum password length for IAM user"
     type = number
+    default = 3
 }
 
 variable "required_numbers" {
     description = "Use numbers or not in IAM password"
     type = bool
+    default = true
 }
