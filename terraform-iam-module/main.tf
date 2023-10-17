9b1db2155ae8025b707d69cb071757492a9632d1
TobiasLiu1990 <tobias.liu@live.se>
2023-10-17 09:49:46 +0000

    a

--- terraform-iam-module/main.tf
+++ terraform-iam-module/main.tf
@@ -1,9 +1,9 @@
-resource "aws_iam_account_alias" "alias" {
-    account_alias = var.alias
+resource "aws_iam_user" "student" {
+    name = "temp"
 }
+
 
 resource "aws_iam_account_password_policy" "strict" {
     minimum_password_length = var.pw_length
     require_symbols = var.required_symbols
-
 }
